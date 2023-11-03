16fad392a86e794540dda7909bcc5994b5f65ffd
teffidev <tefyberal09@gmail.com>
2023-11-03 11:52:44 -0500

    Add searchBox

--- src/components/Map2.jsx
+++ src/components/Map2.jsx
@@ -16,6 +16,7 @@ import {
   GoogleMap,
   Marker,
   Autocomplete,
+  Circle,
 } from "@react-google-maps/api";
 
 const center = { lat: 6.25184, lng: -75.56359 };
@@ -28,71 +29,35 @@ const Map2 = () => {
   });
 
   const [map, setMap] = useState(null);
-  const originRef = useRef();
+  const [origin, setOrigin] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
+
+  const searchBox = useRef(null);
 
   if (!isLoaded) {
     return <SkeletonText />;
   }
 
-  async function calculateRoute() {
-    if (originRef.current.value === "") {
-      return;
+  const handleMapLoad = (map) => {
+    setMap(map);
+  };
+
+  const handlePlaceSelect = () => {
+    const places = searchBox.current.getPlaces();
+    if (places.length > 0) {
+      const place = places[0];
+      const location = {
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng(),
+      };
+      setOrigin(location);
     }
+  };
 
-    const mapElement = document.getElementById("map");
-
-    if (mapElement) {
-      const geocoder = new window.google.maps.Geocoder();
-      const address = originRef.current.value;
-
-      geocoder.geocode({ address }, (results, status) => {
-        if (status === "OK") {
-          const location = results[0].geometry.location;
-          const mapOptions = {
-            center: location,
-            zoom: 15,
-          };
-
-          const map = new window.google.maps.Map(
-            document.getElementById("map"),
-            mapOptions
-          );
-
-          if (map) {
-            map.setMap(null);
-          }
-
-          new window.google.maps.Marker({
-            position: location,
-            map,
-          });
-
-          new window.google.maps.Circle({
-            strokeColor: "#FF0000",
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: "#FF0000",
-            fillOpacity: 0.35,
-            map,
-            center: location,
-            radius: 1000, // meters
-          });
-        } else {
-          alert(
-            "La geocodificación no tuvo éxito por el siguiente motivo: " +
-              status
-          );
-        }
-      });
-    } else {
-      console.error("Elemento con ID 'map' no fue encotrado");
-    }
-  }
-
-  function clearRoute() {
-    console.log("Clearing route");
-    originRef.current.value = "";
-  }
+  const clearOrigin = () => {
+    setOrigin(null);
+    setSearchValue("");
+  };
 
   return (
     <>
@@ -105,8 +70,8 @@ const Map2 = () => {
         <Box position="absolute" left={"20%"} top={"20%"} h="100%" w="80%">
           {/* Google Map Box */}
           <GoogleMap
-            center={center}
             mapContainerStyle={{ height: "100%", width: "100%" }}
+            center={center}
             zoom={10}
             options={{
               zoomControl: false,
@@ -114,8 +79,23 @@ const Map2 = () => {
               mapTypeControl: false,
               fullscreenControl: false,
             }}
-            onLoad={(map) => setMap(map)}>
-            <Marker position={center}></Marker>
+            onLoad={handleMapLoad}>
+            {origin && (
+              <>
+                <Marker position={center}></Marker>
+                <Circle
+                  center={origin}
+                  radius={5000}
+                  options={{
+                    fillColor: "red",
+                    fillOpacity: 0.2,
+                    strokeColor: "red",
+                    strokeOpacity: 1,
+                    strokeWeight: 1,
+                  }}
+                />
+              </>
+            )}
           </GoogleMap>
         </Box>
 
@@ -133,8 +113,14 @@ const Map2 = () => {
           zIndex="1">
           <HStack spacing={2} justifyContent="space-between">
             <Box flexGrow={1}>
-              <Autocomplete>
-                <Input type="text" ref={originRef} />
+              <Autocomplete
+                onLoad={(autocomplete) => (searchBox.current = autocomplete)}>
+                <Input
+                  type="text"
+                  ref={searchBox}
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                />
               </Autocomplete>
             </Box>
 
@@ -143,14 +129,10 @@ const Map2 = () => {
                 <Button
                   colorScheme="blue"
                   type="submit"
-                  onClick={calculateRoute}>
+                  onClick={handlePlaceSelect}>
                   Go
                 </Button>
-                <IconButton
-                  icon={<FaTimes />}
-                  // colorScheme="teal"
-                  onClick={clearRoute}
-                />
+                <IconButton icon={<FaTimes />} onClick={clearOrigin} />
                 <IconButton
                   icon={<FaLocationArrow />}
                   isRound
